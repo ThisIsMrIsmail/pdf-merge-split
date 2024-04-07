63402e41e796c14514c36dacf4897f51d880f653
Ismail Sherif <ismailsherifwork@gmail.com>
2024-04-07 23:20:21 +0200

    Refactor PDF merging code to use PdfWriter instead of merger variable

--- merge.py
+++ merge.py
@@ -1,11 +1,11 @@
 from pypdf import PdfWriter
 import time
 
-merger = PdfWriter()
+writer = PdfWriter()
 
 for pdf in ["pdf-1.pdf", "pdf-2.pdf"]:
-    merger.append(pdf)
+    writer.append(pdf)
 
 nowtime = time.strftime("%Y-%m-%d_%H.%M.%S", time.localtime(time.time()))
-merger.write(f"merged-pdf-{nowtime}.pdf")
-merger.close()
+writer.write(f"merged-pdf-{nowtime}.pdf")
+writer.close()
