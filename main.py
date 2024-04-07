e2a9d2679d9ce33f464db39ee47e625e1b10a7b1
Ismail Sherif <ismailsherifwork@gmail.com>
2024-04-07 22:11:47 +0200

    Add timestamp to merged PDF filename

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from pypdf import PdfWriter
+import time
 
 merger = PdfWriter()
 
 for pdf in ["pdf-1.pdf", "pdf-2.pdf"]:
     merger.append(pdf)
 
-merger.write("merged-pdf.pdf")
+nowtime = time.strftime("%Y-%m-%d_%H.%M.%S", time.localtime(time.time()))
+merger.write(f"merged-pdf-{nowtime}.pdf")
 merger.close()
