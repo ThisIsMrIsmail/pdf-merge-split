a59893e5f8eadb6c86aa2af5bd99f92097462ea1
Ismail Sherif <ismailsherifwork@gmail.com>
2024-04-11 10:34:33 +0200

    Update file paths in pdfiy-merge.ps1 and pdfiy-split.ps1 for cross-platform compatibility

--- pdfiy-merge.ps1
+++ pdfiy-merge.ps1
@@ -1,2 +1,1 @@
-# C:\Users\ismail\dev\side-projects\pdfiy\dist\pdfiy.exe merge $args
-python C:\Users\ismail\dev\side-projects\pdfiy\pdfiy.py merge $args
+C:\Users\ismail\dev\side-projects\pdfiy\dist\pdfiy.exe merge $args
--- pdfiy-split.ps1
+++ pdfiy-split.ps1
@@ -1,2 +1,1 @@
-# C:\Users\ismail\dev\side-projects\pdfiy\dist\pdfiy.exe split $args
-python C:\Users\ismail\dev\side-projects\pdfiy\pdfiy.py split $args
+C:\Users\ismail\dev\side-projects\pdfiy\dist\pdfiy.exe split $args
