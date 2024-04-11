10aa59a8f55684d0ed1bd6cc221e5d43a52d26b4
Ismail Sherif <ismailsherifwork@gmail.com>
2024-04-11 10:32:05 +0200

    Fix order of commands in pdfiy-merge.ps1

--- pdfiy-merge.ps1
+++ pdfiy-merge.ps1
@@ -1,2 +1,2 @@
-python C:\Users\ismail\dev\side-projects\pdfiy\pdfiy.py merge $args
 # C:\Users\ismail\dev\side-projects\pdfiy\dist\pdfiy.exe merge $args
+python C:\Users\ismail\dev\side-projects\pdfiy\pdfiy.py merge $args
