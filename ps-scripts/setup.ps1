240773f3aec8b0ec2cdd3d2301351becc19f3ad3
Ismail Sherif <ismailsherifwork@gmail.com>
2024-04-11 13:10:48 +0200

    Remove unnecessary build and setup files

---
+++ ps-scripts/setup.ps1
@@ -0,0 +1,23 @@
+
+$pdfiy_folder = "C:\Program Files\pdfiy"
+$send_to_dir = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\SendTo"
+
+if (-not (Test-Path $pdfiy_folder)) {
+    New-Item -ItemType Directory -Path $pdfiy_folder
+}
+if (-not (Test-Path $send_to_dir)) {
+    New-Item -ItemType Directory -Path $sendToDir
+}
+
+Copy-Item -Path *.* -Destination $pdfiy_folder
+
+$WshShell = New-Object -comObject WScript.Shell
+
+$MergeShortcut = $WshShell.CreateShortcut("$send_to_dir\Merge with PDFIY.lnk")
+$SplitShortcut = $WshShell.CreateShortcut("$send_to_dir\Split with PDFIY.lnk")
+
+$MergeShortcut.TargetPath = "$pdfiy_folder\pdfiy-merge.exe"
+$SplitShortcut.TargetPath = "$pdfiy_folder\pdfiy-split.exe"
+
+$MergeShortcut.Save()
+$SplitShortcut.Save()
