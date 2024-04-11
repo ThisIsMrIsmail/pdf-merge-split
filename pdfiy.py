acc1afd302a00658d7fefa5466fdf9315a7a16d8
Ismail Sherif <ismailsherifwork@gmail.com>
2024-04-11 11:50:26 +0200

    Refactor file paths and update notifications in pdfiy.py

--- pdfiy.py
+++ pdfiy.py
@@ -1,5 +1,5 @@
 from pypdf import PdfReader, PdfWriter
-from winotify import Notification, audio
+import tkinter.messagebox
 import time
 import sys
 import os
@@ -11,15 +11,12 @@ class pdfiy:
         self.files = files
         pass
     
-    def notify(self, title, msg):
-        notifier = Notification(
-            app_id="PDFIY",
-            title=title,
-            msg=msg,
-            icon="pdfiy.ico",
-            duration="short")
-        notifier.set_audio(audio.Default, loop=False)
-        notifier.show()
+    # def notify(self, title, msg):
+    def notify(self, msg):
+        # notifier = Notification(app_id="PDFIY", title=title, msg=msg, icon="pdfiy.ico", duration="short")
+        # notifier.set_audio(audio.Default, loop=False)
+        # notifier.show()
+        tkinter.messagebox.showerror(title="PDFIY", message=msg, icon="error", type="ok")
 
     def mkdir(self, dir_name):
         if not os.path.exists(dir_name):
@@ -41,14 +38,17 @@ class pdfiy:
         for file in self.files:
             file_ext = os.path.splitext(file)[-1]
             if not os.path.exists(file):
-                self.notify(os.path.basename(file), "File does not exist.")
+                self.notify(f"{os.path.basename(file)} File does not exist.")
+                # self.notify(os.path.basename(file), "File does not exist.")
                 continue
             if not self.check_if_pdf(file_ext):
-                self.notify(os.path.basename(file), "File is not a valid PDF file.")
+                self.notify(f"{os.path.basename(file)} File is not a valid PDF file.")
+                # self.notify(os.path.basename(file), "File is not a valid PDF file.")
                 continue
             self.writer.append(file)
         if len(self.writer.pages) == 0:
-            self.notify(os.path.basename(file), "No pages to merge.")
+            self.notify(f"{os.path.basename(file)} No pages to merge.")
+            # self.notify(os.path.basename(file), "No pages to merge.")
             return
         nowtime = time.strftime("%Y-%m-%d_%H.%M.%S", time.localtime(time.time()))
         self.writer.write(f"{dir_name}\\{file_name}-merged-{nowtime}.pdf")
@@ -65,17 +65,20 @@ class pdfiy:
 
     def split_execute(self):
         if len(self.files) == 0:
-            self.notify(os.path.basename(file), "File is not a valid PDF file.")
+            self.notify(f"{os.path.basename(file)} File is not a valid PDF file.")
+            # self.notify(os.path.basename(file), "File is not a valid PDF file.")
         for file in self.files:
             if not os.path.exists(file):
-                self.notify(os.path.basename(file), "File does not exist.")
+                self.notify(f"{os.path.basename(file)} File does not exist.")
+                # self.notify(os.path.basename(file), "File does not exist.")
                 continue
             file_list = os.path.splitext(file)
             dir_name = file_list[0]
             file_name = os.path.basename(file_list[0])
             file_ext = file_list[-1]
             if not self.check_if_pdf(file_ext):
-                self.notify(os.path.basename(file), "File is not a valid PDF file.")
+                self.notify(f"{os.path.basename(file)} File is not a valid PDF file.")
+                # self.notify(os.path.basename(file), "File is not a valid PDF file.")
                 continue
             nowtime = time.strftime("%Y-%m-%d_%H.%M.%S", time.localtime(time.time()))
             output_dir = f"{dir_name}-splitted-{nowtime}"
@@ -90,12 +93,14 @@ if __name__ == "__main__":
     pdfiy = pdfiy(files)
 
     if len(files) == 0:
-        pdfiy.notify(os.path.basename(files), "Please, select at least one  PDF file.")
+        pdfiy.notify("Please, select at least one  PDF file.")
+        # pdfiy.notify("", "Please, select at least one  PDF file.")
 
     if op == "merge":
         pdfiy.merge()
     elif op == "split":
         pdfiy.split_execute()
     else:
-        pdfiy.notify(os.path.basename(files), "Not valid operation.")
+        pdfiy.notify(f"{os.path.basename(files)}: Not a valid operation.")
+        pdfiy.notify(os.path.basename(files), "Not a valid operation.")
     pass
