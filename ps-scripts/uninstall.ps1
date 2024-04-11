1f2bba313f91c754361d59ab2bdaec697d07c14e
Ismail Sherif <ismailsherifwork@gmail.com>
2024-04-11 13:34:15 +0200

    Update PDFiy uninstall script and file paths

--- ps-scripts/uninstall.ps1
+++ ps-scripts/uninstall.ps1
@@ -1,3 +1,8 @@
+$send_to_dir = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\SendTo"
+
+$MergeShortcut = $WshShell.CreateShortcut("$send_to_dir\Merge with PDFIY.lnk")
+$SplitShortcut = $WshShell.CreateShortcut("$send_to_dir\Split with PDFIY.lnk")
+
 Get-ChildItem -Path C:\pdfiy -Include *.* -File -Recurse | foreach { $_.Delete()}
-Remove-Item -Recurse -Force C:\pdfiy
 Remove-Item $PSCommandPath -Force
+Remove-Item -Recurse -Force C:\pdfiy
